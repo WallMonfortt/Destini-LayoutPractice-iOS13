634d3188aa61230c0eaf3355de2ca0cd0f9c186d
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-10-17 17:54:39 -0600

    Challenge solved

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var storyBrain = StoryBrain()
+    var timer: Timer!
+    @IBOutlet weak var questionTextView: UILabel!
+    @IBOutlet weak var choiceOneView: UIButton!
+    @IBOutlet weak var choiceTwoView: UIButton!
+    
+    
+    @IBAction func choiceSelected(_ sender: UIButton) {
+        let choiceDestination = sender.tag
+        print("choice destination: \(choiceDestination)")
+        storyBrain.updateDastination(choiceDestination)
+        
+        sender.backgroundColor = UIColor.darkGray
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
+            self.updateTexts()
+             }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateTexts()
 
     }
+    
+    func updateTexts() {
+        let storyTexts = storyBrain.getTexts()
+        questionTextView.text = storyTexts.title
+        choiceOneView.setTitle(storyTexts.choice1, for: .normal)
+        choiceOneView.tag = storyTexts.choiceDestination1
+        choiceTwoView.setTitle(storyTexts.choice2, for: .normal)
+        choiceTwoView.tag = storyTexts.choiceDestination2
+        choiceOneView.backgroundColor = .clear
+        choiceTwoView.backgroundColor = .clear
+    }
+    
 
 
 }
